66a230a3af240d5d86c3dd5d52a8f81c30d3deb1
katarzynanosal6 <katarzynan101@wp.pl>
2026-03-14 14:21:05 +0100

    folder lab1

---
+++ lab1/mojabiblioteka/Kalkulator.cs
@@ -0,0 +1,28 @@
+﻿namespace mojabiblioteka;
+
+public class Kalkulator
+{
+    public int Dodaj(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Odejmij(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Pomnoz(int a, int b)
+    {
+        return a * b;
+    }
+
+    public double Podziel(int a, int b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException("Nie można dzielić przez zero.");
+        }
+        return (double)a / b;
+    }
+}
